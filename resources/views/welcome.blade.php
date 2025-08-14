3c2116d9d94edf77ced5aaf8bcc6ac3876528577
AgostaSalvatore <agosta.boolean@gmail.com>
2025-08-14 22:06:22 +0200

    feat: create train listing page with cyberpunk styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,65 +10,68 @@
     </h1>
 </div>
 
-<ul>
-    @foreach ($trains as $train)
-        <div class="card">
-            <div class="card-top d-flex justify-content-around">
-                <div class="card-top-left">
-                    <h2>
-                        Azienda: 
-                        {{$train -> company}}
-                    </h2>
+<div class="container">
+
+    <ul>
+        @foreach ($trains as $train)
+            <div class="card">
+                <div class="card-top d-flex justify-content-around">
+                    <div class="card-top-left">
+                        <h2>
+                            Azienda: 
+                            {{$train -> company}}
+                        </h2>
+                    </div>
+                    <div class="card-top-right">
+                        <h2>
+                            ID treno: 
+                            {{$train -> id}}
+                        </h2>
+                    </div>
                 </div>
-                <div class="card-top-right">
-                    <h2>
-                        ID treno: 
-                        {{$train -> id}}
-                    </h2>
+                <div class="card-body">
+                    <p>
+                        Stazione di partenza: 
+                        {{$train -> departure_station}}
+                    </p>
+                    <p>
+                        Stazione di arrivo: 
+                        {{$train -> arrival_station}}
+                    </p>
+                    <p>
+                        Orario di partenza: 
+                        {{$train -> departure_time}}
+                    </p>
+                    <p>
+                        Orario di arrivo: 
+                        {{$train -> arrival_time}}
+                    </p>
+                    <p>
+                        Codice treno: 
+                        {{$train -> train_code}}
+                    </p>
+                    <p>
+                        Carrozze: 
+                        {{$train -> total_carriages}}
+                    </p>
+                    <p>
+                        @if ($train -> on_time_or_not == 0)
+                            In orario
+                        @else
+                            Non in orario
+                        @endif
+                    </p>
+                    <p>
+                        @if ($train -> cancelled_or_not == 0)
+                            Non cancellato
+                        @else
+                            Cancellato
+                        @endif
+                    </p>
                 </div>
             </div>
-            <div class="card-body">
-                <p>
-                    Stazione di partenza: 
-                    {{$train -> departure_station}}
-                </p>
-                <p>
-                    Stazione di arrivo: 
-                    {{$train -> arrival_station}}
-                </p>
-                <p>
-                    Orario di partenza: 
-                    {{$train -> departure_time}}
-                </p>
-                <p>
-                    Orario di arrivo: 
-                    {{$train -> arrival_time}}
-                </p>
-                <p>
-                    Codice treno: 
-                    {{$train -> train_code}}
-                </p>
-                <p>
-                    Carrozze: 
-                    {{$train -> total_carriages}}
-                </p>
-                <p>
-                    @if ($train -> on_time_or_not == 0)
-                        In orario
-                    @else
-                        Non in orario
-                    @endif
-                </p>
-                <p>
-                    @if ($train -> cancelled_or_not == 0)
-                        Non cancellato
-                    @else
-                        Cancellato
-                    @endif
-                </p>
-            </div>
-        </div>
-    @endforeach
-</ul>
+        @endforeach
+    </ul>
+</div>
 
 @endsection
